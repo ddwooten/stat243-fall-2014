c332854c6daf2f0df1d9d5c341145b42f9ee7c45
ddwooten <ddwooten@rocketmail.com>
2014-09-25 19:59:41 -0700

    Added some short stuff to A.R

--- HW/HW3/A.R
+++ HW/HW3/A.R
@@ -3,8 +3,15 @@
 #Class STATS243
 #PS3
 
-#The first line makes this file callable from the command line,
-#assuming you've given it executable permissions
+#The first line makes this file callable from the command 
+#line,assuming you've given it executable permissions
 #Comments will explain how this code works
 
+#This line loads the required libraries and packages that
+# we want to use
+library(scrapeR)
 
+#This line uses scrapeR's retreival function to store
+#the websites html file in an R object
+orginal_html <- scrape(url='www.presidency.ucb.edu/sou.php#axzz265cEKp1a'+
+, 
