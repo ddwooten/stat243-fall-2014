2d5ac30961e8e9d2e89c664c64421236e41ff690
ddwooten <ddwooten@rocketmail.com>
2014-09-25 18:51:11 -0700

    Added HW2 tex file and intro file for script for HW3

---
+++ HW/HW3/A.R
@@ -0,0 +1,10 @@
+#!/usr/bin/Rscript
+#Creator: Daniel Wooten
+#Class STATS243
+#PS3
+
+#The first line makes this file callable from the command line,
+#assuming you've given it executable permissions
+#Comments will explain how this code works
+
+
