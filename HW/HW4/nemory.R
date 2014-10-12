8d91a7f597dc4f3ecb980869266528a5db8a9bb2
ddwooten <ddwooten@rocketmail.com>
2014-10-12 13:55:02 -0700

    Problem 3 finished, 2 still needs help with plotting. Begining 4 now.

---
+++ HW/HW4/nemory.R
@@ -0,0 +1,40 @@
+#!/usr/bin/Rscript
+#Creator: Daniel Wooten
+#Class STATS243
+#PS4
+
+# A function to make prettier output
+cep <- function(){
+	cat('********************************************************** \n')
+}
+cep()
+cat('BEGIN EXECUTION \n')
+
+cat('The List \n')
+
+v1 <- c(1,2,3)
+v2 <- c(4,5,6)
+v3 <- c(7,8,9)
+df1 <- vector('list',3)
+df1[[1]] <- v1
+df1[[2]] <- v2
+df1[[3]] <- v3
+
+cep()
+print(df1)
+cep()
+cat('The output of .Internal(inspect(df1)) \n')
+cep()
+.Internal(inspect(df1))
+cep()
+df1[[1]][1] <- 10
+cep()
+cat('The output of .Internal(inspect(df1)) after modificaiton \n')
+cep()
+.Internal(inspect(df1))
+cep()
+cat('The output of .Internal(inspect(df1[1])) without other interference \n')
+.Internal(inspect(df1[[1]]))
+cep()
+cat('END EXECUTION \n')
+cep()
