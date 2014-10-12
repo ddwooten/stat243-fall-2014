3e801d79086e242761ea5767a2996e465140ff46
ddwooten <ddwooten@rocketmail.com>
2014-10-12 14:37:09 -0700

    Added crap in walk

--- HW/HW4/RandomWalk.R
+++ HW/HW4/RandomWalk.R
@@ -13,7 +13,7 @@ cep()
 cat('BEGIN EXECUTION \n')
 
 #Initialize the random walk function
-Priase_Be_To_The_RNG = function(n)
+Priase_Be_To_The_RNG = function(n,WholeWalk)
 {
 #These are the possible x steps we can take
 	xsteps <- c(-1,1,0,0)
@@ -22,11 +22,13 @@ Priase_Be_To_The_RNG = function(n)
 #This vector represents the steps (or their correlation
 # to our steps vectors) that we will take
 	step <- sample(1:4,n-1,replace=T)
+#This if statement will handle the two cases
+#whole walk, or just the final position
+	if(
 #These next two lines will create vectors of the new
 # x and y position at each step
 	posx <- c(0,CumSum(xsteps[step]))
 	posy <- c(0,CumSum(ystep[step]))
-# This 
 
 cat('END EXECUTION \n')
 cep()
