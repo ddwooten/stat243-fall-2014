a149cb1c9142dca2689b3060c8f968cf230a6642
ddwooten <ddwooten@rocketmail.com>
2014-10-14 16:03:16 -0700

    Timing r working. About to remove 20% and 40% for clarity

--- HW/HW4/timing.R
+++ HW/HW4/timing.R
@@ -36,35 +36,47 @@ for(j in seq(0,size,1000) )
 	s5 <- as.integer(i*50)
 	vec <- rnorm(length)
 	bol1 <- sample(boolvec,s1,replace=T)
-#	bol2 <- sample(boolvec,s2,replace=T)
-#	bol3 <- sample(boolvec,s3,replace=T)
-#	bol4 <- sample(boolvec,s4,replace=T)
+	bol2 <- sample(boolvec,s2,replace=T)
+	bol3 <- sample(boolvec,s3,replace=T)
+	bol4 <- sample(boolvec,s4,replace=T)
 	bol5 <- sample(boolvec,s5,replace=T)
 	int1 <- sample(1:length,s1,replace=F)
-#	int2 <- sample(1:length,s2,replace=F)
-#	int3 <- sample(1:length,s3,replace=F)
-#	int4 <- sample(1:length,s4,replace=F)
+	int2 <- sample(1:length,s2,replace=F)
+	int3 <- sample(1:length,s3,replace=F)
+	int4 <- sample(1:length,s4,replace=F)
 	int5 <- sample(1:length,s5,replace=F)
 
 	ivec[i] <- length	
 	itimes1[i] <- system.time(vec[int1])[3] 
-#	itimes2[i] <- system.time(vec[int2])[3]
-#	itimes3[i] <- system.time(vec[int3])[3]
-#	itimes4[i] <- system.time(vec[int4])[3]
+	itimes2[i] <- system.time(vec[int2])[3]
+	itimes3[i] <- system.time(vec[int3])[3]
+	itimes4[i] <- system.time(vec[int4])[3]
 	itimes5[i] <- system.time(vec[int5])[3]
 	btimes1[i] <- system.time(vec[bol1])[3]
-#	btimes2[i] <- system.time(vec[bol1])[3]
-#	btimes3[i] <- system.time(vec[bol1])[3]
-#	btimes4[i] <- system.time(vec[bol1])[3]
+	btimes2[i] <- system.time(vec[bol1])[3]
+	btimes3[i] <- system.time(vec[bol1])[3]
+	btimes4[i] <- system.time(vec[bol1])[3]
 	btimes5[i] <- system.time(vec[bol1])[3]
 }
 
-plot(ivec,itimes1,xlab='Vector Size',ylab='Elapsed Time (s)', 
-	main='Time to Subset a Vector with Indicies')
-points(ivec,itimes5,col=2,pch=18)
-par(new=F)
 pdf('ps4plots.pdf')
-dev.off()
+plot(ivec,itimes1,xlab='Vector Size',ylab='Elapsed Time (s)', 
+	main='Time to Subset a Vector with Indicies',col=1)
+points(ivec,itimes2,col=2,pch=18)
+points(ivec,itimes3,col=3,pch=18)
+points(ivec,itimes4,col=4,pch=18)
+points(ivec,itimes5,col=5,pch=18)
+points(ivec,btimes1,col=6,pch=18)
+points(ivec,btimes2,col=7,pch=18)
+points(ivec,btimes3,col=8,pch=18)
+points(ivec,btimes4,col=9,pch=18)
+points(ivec,btimes5,col=10,pch=18)
+legend("topleft",c("Index 10% of Vector","Index 20% of Vector",
+	"Index 30% of Vector","Index 40% of Vector","Index 50% of Vector",
+	"Bool 10% of Vector", "Bool 20% of Vector","Bool 30% of Vector",
+	"Bool 40% of Vector","Bool 50% of Vector"),
+	pch=18, col=c(1,2,3,4,5,6,7,8,9,10))
+graphics.off()
 cat(length(itimes1))
 cat('\n')
 cat(length(itimes2))
