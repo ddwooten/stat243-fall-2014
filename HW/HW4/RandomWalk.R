e8c6b60563230c7a32c055e642f60f30ff40b4d7
ddwooten <ddwooten@rocketmail.com>
2014-10-13 20:58:21 -0700

    Randomwalk written, ready for first testing

--- HW/HW4/RandomWalk.R
+++ HW/HW4/RandomWalk.R
@@ -12,16 +12,20 @@ cep <- function()
 cep()
 cat('BEGIN EXECUTION \n')
 
+#Collect the function arguments
+num <- readlline(prompt='Enter the number of steps: ')
+YesWalk <- readline(prompt='0 for end position, 1 for whole walk: ')
+
 #Initialize the random walk function
-Priase_Be_To_The_RNG = function(n,WholeWalk=NULL)
+Priase_Be_To_The_RNG = function(N,WholeWalk=NULL)
 {
 #Try to cast inpput as integer
-	N <- try(as.integer(n))
+	n <- try(as.integer(N))
 #If input can be cast as integer proceed
-	if(!class(N) == 'try-error')
+	if(!class(n) == 'try-error')
 	{
 #Check to make sure N is actually an integer
-		if(!is.integer(N) | n <= 0 | is.na(N) )
+		if(!is.integer(n) | n <= 0 | is.na(n) )
 		{
 #If N is not an integer, bork and exit with message
 			cat('ERROR!!: First argument n must
@@ -63,14 +67,34 @@ Priase_Be_To_The_RNG = function(n,WholeWalk=NULL)
 	ysteps <- c(0,0,-1,1)
 #This vector represents the steps (or their correlation
 # to our steps vectors) that we will take
-	step <- sample(1:4,n-1,replace=T)
-#This if statement will handle the two cases
-#whole walk, or just the final position
-	if(
+	steps <- sample(1:4,n,replace=T)
 #These next two lines will create vectors of the new
 # x and y position at each step
-	posx <- c(0,CumSum(xsteps[step]))
-	posy <- c(0,CumSum(ystep[step]))
+		posx <- c(sx,cumsum(xsteps[steps]))
+		posy <- c(sy,cumsum(ystep[steps]))
+#This if statement will handle the two cases
+#whole walk, or just the final position
+	if(WholeWalk == 1)
+	{
+		walk_map <- matrix(ncol = 2, nrow = n + 1 )
+		walk_map[,1] <- posx
+		walk_map[,2] <- posy
+	}
+	else
+	{
+		walk_map <- vector('numeric',2)
+		walk_map[1] <- posx[n+1]
+		walk_map[2] <- posy[n+1]
+	}
+	return(walk_map)
+}
 
+cep()
+cat('The result\n')
+cep()
+cat(walk_map)
+cat('\n')
+cep()
+		
 cat('END EXECUTION \n')
 cep()
