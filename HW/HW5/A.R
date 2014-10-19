49b06577f661135ed903ac9fd99d331e9684ce82
ddwooten <ddwooten@rocketmail.com>
2014-10-19 16:44:09 -0700

    Created template for script and added git ignore

---
+++ HW/HW5/A.R
@@ -0,0 +1,17 @@
+#!/usr/bin/Rscript
+#Creator: Daniel Wooten
+#Class STATS243
+#PS4
+
+# A function to make prettier output
+cep <- function()
+{
+	cat('********************************************************** \n')
+}
+
+cep()
+cat('BEGIN EXECUTION!!!')
+load("ps4prob6.Rda") 
+
+cat('END EXECUTION!!')
+cep()
