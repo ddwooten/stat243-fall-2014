d90bf27a71d15bfef24f7846845103da6ffb982a
ddwooten <ddwooten@rocketmail.com>
2014-10-20 17:51:24 -0700

    A.R is computing something. I don't know what

--- HW/HW5/A.R
+++ HW/HW5/A.R
@@ -10,8 +10,25 @@ cep <- function()
 }
 
 cep()
-cat('BEGIN EXECUTION!!!')
+cat('BEGIN EXECUTION!!!\n')
 load("data.Rda") 
 
+#Go ahead and transpose x cause we need to
+X_trans <- t(X)
+
+#Initalize p as its good practice
+p <- vector('double',length(X))
+
+#Solve for p the naieve way
+p = (X_trans*beta)/(1+X_trans*beta)
+
+#Solve for the liklihood
+likelihood <- prod(dbinom(y,n,p))
+
+#Print out for checking
+cep()
+cat(length(p))
+cat('\n')
+cep()
 cat('END EXECUTION!!')
 cep()
